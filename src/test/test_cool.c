0c66e152bdd585068ec3b2268cf64a81f08e3c7b
Christopher Wood <christopherwood07@gmail.com>
2015-08-07 09:01:30 -0700

    Makefile and cmocka test library

---
+++ src/test/test_cool.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+
+int 
+main(int argc, char **argv) 
+{
+
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
