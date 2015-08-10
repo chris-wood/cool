228f4475dd4ccec4d0f3d24d7ba753e12abedc9d
Christopher Wood <christopherwood07@gmail.com>
2015-08-10 15:43:19 -0700

    refactoring cool

--- src/test/test_cool.c
+++ src/test/test_cool.c
@@ -7,7 +7,7 @@
 #include "../cool.c"
 #include "../mpc.h"
 
-static void test_cval_longInteger(void **state) {
+static void test_Value_longInteger(void **state) {
 	long input = 10;
     cval *value = cval_longInteger(input);
     assert_true(value->type == CoolValue_LongInteger);
@@ -15,11 +15,35 @@ static void test_cval_longInteger(void **state) {
     assert_true(value->cell == NULL);
 }
 
+static void test_Value_double(void **state) {
+	double input = 1337.0;
+    cval *value = Value_double(input);
+    assert_true(value->type == CoolValue_Double);
+    assert_true(value->fpnumber == input);
+    assert_true(value->cell == NULL);
+}
+
+static void test_Value_byte(void **state) {
+	uint8_t input = 0xBEEF;
+    cval *value = Value_byte(input);
+    assert_true(value->type == CoolValue_Byte);
+    assert_true(value->number == input);
+    assert_true(value->cell == NULL);
+}
+
+static void test_Value_string(void **state) {
+	char *input = "COOL";
+    cval *value = Value_string(input);
+    assert_true(value->type == CoolValue_String);
+    assert_true(memcmp(input, value->string, strlen(input)) == 0);
+    assert_true(value->cell == NULL);
+}
+
 int 
 main(int argc, char **argv) 
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(test_cval_longInteger),
+        cmocka_unit_test(test_Value_longInteger),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
