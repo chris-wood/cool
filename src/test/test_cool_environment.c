3bbe7d33388198fc76a969837e2908127aced465
Christopher Wood <christopherwood07@gmail.com>
2015-08-12 16:09:15 -0700

    updates

---
+++ src/test/test_cool_environment.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "../cool.c"
+#include "../mpc.h"
+
+// Environment *Environment_new();
+// void Environment_delete(Environment *env);
+// Value *Environment_get(Environment *env, Value *val);
+// void Environment_put(Environment *env, Value* key, Value *val);
+// Environment *Environment_copy(Environment *env);
+// void Environment_def(Environment *env, Value *key, Value *value)
+
+static void test_Environment_new(void **state) {
+    // TODO
+    // assert_true(value->type == CoolValue_LongInteger);
+}
+
+int 
+main(int argc, char **argv) 
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(test_Environment_new),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
