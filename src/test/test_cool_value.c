63e0a93a55d948a9bc0a350a5b30a4e4ea07c863
Christopher Wood <christopherwood07@gmail.com>
2015-08-12 16:09:40 -0700

    removed old, in with the new

--- src/test/test_cool_value.c
+++ src/test/test_cool_value.c
@@ -3,12 +3,11 @@
 #include <setjmp.h>
 #include <cmocka.h>
 
-// For coverage
 #include "../cool.c"
 #include "../mpc.h"
 
 static void test_Value_longInteger(void **state) {
-	long input = 10;
+    long input = 10;
     cval *value = cval_longInteger(input);
     assert_true(value->type == CoolValue_LongInteger);
     assert_true(value->number == input);
@@ -16,7 +15,7 @@ static void test_Value_longInteger(void **state) {
 }
 
 static void test_Value_double(void **state) {
-	double input = 1337.0;
+    double input = 1337.0;
     cval *value = Value_double(input);
     assert_true(value->type == CoolValue_Double);
     assert_true(value->fpnumber == input);
@@ -24,7 +23,7 @@ static void test_Value_double(void **state) {
 }
 
 static void test_Value_byte(void **state) {
-	uint8_t input = 0xBEEF;
+    uint8_t input = 0xBEEF;
     cval *value = Value_byte(input);
     assert_true(value->type == CoolValue_Byte);
     assert_true(value->number == input);
@@ -32,7 +31,7 @@ static void test_Value_byte(void **state) {
 }
 
 static void test_Value_string(void **state) {
-	char *input = "COOL";
+    char *input = "COOL";
     cval *value = Value_string(input);
     assert_true(value->type == CoolValue_String);
     assert_true(memcmp(input, value->string, strlen(input)) == 0);
