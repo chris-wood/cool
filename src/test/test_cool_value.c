1ec3a2da39eeb6ce7d7ba6e6e73e1928ec98b14e
Christopher Wood <christopherwood07@gmail.com>
2015-08-15 08:49:53 -0700

    Adding more tests

--- src/test/test_cool_value.c
+++ src/test/test_cool_value.c
@@ -9,43 +9,83 @@
 static void test_value_Integer(void **state) {
     long input = 10;
     Value *value = value_Integer(input);
+
     assert_true(value->type == CoolValue_Integer);
     assert_true(value->number == input);
     assert_true(value->cell == NULL);
+
+    value_Delete(value);
 }
 
 static void test_value_Double(void **state) {
     double input = 1337.0;
     Value *value = value_Double(input);
+
     assert_true(value->type == CoolValue_Double);
     assert_true(value->fpnumber == input);
     assert_true(value->cell == NULL);
+
+    value_Delete(value);
 }
 
 static void test_value_Byte(void **state) {
     uint8_t input = (uint8_t) 0xAB;
     Value *value = value_Byte(input);
+
     assert_true(value->type == CoolValue_Byte);
     assert_true(value->byte == input);
     assert_true(value->cell == NULL);
+
+    value_Delete(value);
 }
 
 static void test_value_String(void **state) {
     char *input = "COOL";
     Value *value = value_String(input);
+
     assert_true(value->type == CoolValue_String);
     assert_true(memcmp(input, value->string, strlen(input)) == 0);
     assert_true(value->cell == NULL);
+
+    value_Delete(value);
 }
 
-int 
-main(int argc, char **argv) 
+static void test_value_Print(void **state) {
+    long expected = 10L;
+    Value *value = value_Integer(expected);
+    FILE *fhandle = fopen("test.tmp", "w");
+    value_Print(fhandle, value);
+    fclose(fhandle);
+
+    fhandle = fopen("test.tmp", "r");
+    char *numberString = (char *) malloc(sizeof(long) + 1);
+    fgets(numberString, sizeof(long) + 1, fhandle);
+    long actual = atol(numberString);
+
+    free(numberString);
+
+    assert_true(actual == expected);
+
+    value_Delete(value);
+}
+
+static void test_value_Error(void **state) {
+    char *expected = "Testing error messages";
+    Value *error = value_Error(expected);
+    assert_true(strcmp(expected, error->errorString) == 0);
+    value_Delete(error);
+}
+
+int
+main(int argc, char **argv)
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_value_Integer),
         cmocka_unit_test(test_value_Double),
         cmocka_unit_test(test_value_Byte),
-        cmocka_unit_test(test_value_String)
+        cmocka_unit_test(test_value_String),
+        cmocka_unit_test(test_value_Print),
+        cmocka_unit_test(test_value_Error)
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
